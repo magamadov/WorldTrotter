cd99bbf3ad2c7a8c445c93e970fca9365eda1f19
Zelimkhan Magamadov <magamadov@gmail.com>
2020-06-13 00:13:18 +0300

    Silver and Gold challenge completed.

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -9,11 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
- 
-    
-
-
-}
-
+  let gradientLayer = CAGradientLayer()
   
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    showGradient()
+    
+  }
+  
+  override func viewWillLayoutSubviews() {
+    super.viewWillLayoutSubviews()
+    gradientLayer.frame = view.bounds
+  }
+  
+  func showGradient() {
+    gradientLayer.frame = view.bounds
+    gradientLayer.colors = [
+      UIColor.red.cgColor,
+      UIColor.orange.cgColor,
+      UIColor.yellow.cgColor,
+      UIColor.green.cgColor,
+      UIColor.blue.cgColor,
+      UIColor.purple.cgColor
+    ]
+    
+    gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+    gradientLayer.endPoint = CGPoint(x: 0, y: 1)
+    view.layer.insertSublayer(gradientLayer, at: 0)
+  }
+}
