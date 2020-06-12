d86682e5f9135ac63d3616d2d1ab47e5276b39ad
Zelimkhan Magamadov <magamadov@gmail.com>
2020-06-12 23:49:15 +0300

    Bronze challange, Auto Layout practice.

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by ZELIMKHAN MAGAMADOV on 11.06.2020.
+//  Copyright © 2020 ZELIMKHAN MAGAMADOV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+ 
+    
+
+
+}
+
+  
